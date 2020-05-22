ad4b054982310d3510922f59a9f58f1c19cc6203
Informatica Arelux <informatica@arelux.com>
2020-05-22 08:37:08 +0200

    https://grupoarelux.atlassian.net/browse/ODOOARELUX-758

--- aws_sms_shipping_expedition/models/shipping_expedition.py
+++ aws_sms_shipping_expedition/models/shipping_expedition.py
@@ -15,102 +15,55 @@ class ShippingExpedition(models.Model):
     @api.one    
     def action_custom_send_sms_info_slack(self):
         return True
-    
+
     @api.one
-    def action_send_sms_info(self, sms_template_id=False):
-        if sms_template_id!=False:
-            states_skip = ['error', 'generate','canceled', 'delivered']
-            if self.state not in states_skip:        
-                if self.date_send_sms_info==False:
-                    if self.delegation_name!=False and self.delegation_phone!=False:
-                        allow_send_sms = True
-                        
-                        if self.partner_id.mobile==False:
-                            allow_send_sms = False
-                            
-                        if allow_send_sms==True:
-                            sms_template_item = self.env['sms.template'].search([('id', '=', sms_template_id)])[0]                                
-                            sms_compose_message_vals = {
-                                'model': 'shipping.expedition',
-                                'res_id': self.id,
-                                'mobile': self.partner_id.mobile,
-                                'sms_template_id': sms_template_id
-                            }
-                            #Fix user_id
-                            if self.user_id.id>0:
-                                self.env.user.id = self.user_id.id
-                                sms_compose_message_obj = self.env['sms.compose.message'].sudo(self.env.user.id).create(sms_compose_message_vals)
-                            else:
-                                sms_compose_message_obj = self.env['sms.compose.message'].sudo().create(sms_compose_message_vals)
-                                
-                            return_onchange_sms_template_id = sms_compose_message_obj.onchange_sms_template_id(sms_template_item.id, 'shipping.expedition', self.id)
-                            
-                            sms_compose_message_obj.update({
-                                'sender': return_onchange_sms_template_id['value']['sender'],
-                                'message': return_onchange_sms_template_id['value']['message']                                                     
-                            })
-                            sms_compose_message_obj.send_sms_action()
-                            
-                            if sms_compose_message_obj.action_send==True:
-                                #save_log
-                                arelux_automation_log_vals = {                    
+    def action_send_sms_info(self):
+        allow_send = False
+        if self.carrier_id.sms_info_sms_template_id.id > 0:
+            if self.date_send_sms_info == False:
+                if self.carrier_id.send_sms_info == True:
+                    if self.carrier_id.sms_info_sms_template_id != False:
+                        if self.state not in ['error', 'generate', 'canceled', 'delivered', 'incidence']:
+                            if self.partner_id.mobile != False and self.partner_id.mobile_code_res_country_id != False:
+                                if self.carrier_id.carrier_type == 'nacex':
+                                    allow_send = True
+                                else:
+                                    if self.delegation_name != False and self.delegation_phone != False:
+                                        allow_send = True
+                            # allow_send
+                            if allow_send == True:
+                                _logger.info('Enviamos el SMS respecto a la expedicion ' + str(self.id))
+                                sms_compose_message_vals = {
                                     'model': 'shipping.expedition',
                                     'res_id': self.id,
-                                    'category': 'shipping_expedition',
-                                    'action': 'send_sms',                                                                                                                                                                                           
+                                    'country_id': self.partner_id.mobile_code_res_country_id.id,
+                                    'mobile': self.partner_id.mobile,
+                                    'sms_template_id': self.carrier_id.sms_info_sms_template_id.id
                                 }
-                                arelux_automation_log_obj = self.env['arelux.automation.log'].sudo().create(arelux_automation_log_vals)
-                                #other                                                                                                                                                         
-                                self.date_send_sms_info = datetime.today()
-                                self.action_custom_send_sms_info_slack()#Fix Slack                                                
-                                            
+                                # Fix user_id
+                                if self.user_id.id > 0:
+                                    sms_compose_message_obj = self.env['sms.compose.message'].sudo(
+                                        self.user_id.id).create(sms_compose_message_vals)
+                                else:
+                                    sms_compose_message_obj = self.env['sms.compose.message'].sudo().create(
+                                        sms_compose_message_vals)
+
+                                return_onchange_sms_template_id = sms_compose_message_obj.onchange_sms_template_id(
+                                    self.carrier_id.sms_info_sms_template_id.id, 'shipping.expedition', self.id)
+
+                                sms_compose_message_obj.update({
+                                    'sender': return_onchange_sms_template_id['value']['sender'],
+                                    'message': return_onchange_sms_template_id['value']['message']
+                                })
+                                sms_compose_message_obj.send_sms_action()
+
+                                if sms_compose_message_obj.action_send == True:
+                                    # other
+                                    self.date_send_sms_info = datetime.today()
+                                    self.action_custom_send_sms_info_slack()  # Fix Slack
+
             return True
-            
-    @api.multi    
-    def cron_shipping_expedition_cbl_send_sms_info(self, cr=None, uid=False, context=None):
-        cbl_expedition_sms_info_template_id = self.env['ir.config_parameter'].sudo().get_param('cbl_expedition_sms_info_template_id')
-        
-        shipping_expedition_ids = self.env['shipping.expedition'].search(
-            [
-                ('carrier_id.carrier_type', '=', 'cbl'),
-                ('state', 'not in', ('error', 'generate','canceled', 'delivered')),
-                ('date_send_sms_info', '=', False),
-                ('delegation_name', '!=', False),
-                ('delegation_phone', '!=', False),
-                ('partner_id.mobile', '!=', False),
-                ('partner_id.mobile', 'like', '+'),
-            ]
-        )
-        if len(shipping_expedition_ids)>0:
-            for shipping_expedition_id in shipping_expedition_ids:
-                shipping_expedition_id.action_send_sms_info(cbl_expedition_sms_info_template_id)                                
-        
-    @api.one    
+
+    @api.one
     def action_generate_shipping_expedition_link_tracker(self):
         return super(ShippingExpedition, self).action_generate_shipping_expedition_link_tracker()
-    
-    @api.one 
-    def cron_shipping_expedition_txt_send_sms_info_item(self, template_id):
-        #Fix link_tracker_id
-        if self.link_tracker_id.id==0:
-            res = self.action_generate_shipping_expedition_link_tracker()
-            
-        self.action_send_sms_info(template_id)
-    
-    @api.multi
-    def cron_shipping_expedition_txt_send_sms_info(self, cr=None, uid=False, context=None):
-        txt_expedition_sms_info_template_id = self.env['ir.config_parameter'].sudo().get_param('txt_expedition_sms_info_template_id')
-        
-        shipping_expedition_ids = self.env['shipping.expedition'].search(
-            [
-                ('carrier_id.carrier_type', '=', 'txt'),
-                ('state', 'not in', ('error', 'generate','canceled', 'delivered')),
-                ('date_send_sms_info', '=', False),
-                ('delegation_name', '!=', False),
-                ('delegation_phone', '!=', False),
-                ('partner_id.mobile', '!=', False),
-            ]
-        )
-        if len(shipping_expedition_ids)>0:
-            for shipping_expedition_id in shipping_expedition_ids:
-                shipping_expedition_id.cron_shipping_expedition_txt_send_sms_info_item(txt_expedition_sms_info_template_id)                                                                                                                                                                                                                                                    
