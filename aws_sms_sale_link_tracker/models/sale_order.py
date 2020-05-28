c31ca78b9dcf78bf97f7342358d0a2f367ee8850
Informatica Arelux <informatica@arelux.com>
2020-05-28 11:04:46 +0200

    aws_sms_sale_link_tracker

---
+++ aws_sms_sale_link_tracker/models/sale_order.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import logging
+_logger = logging.getLogger(__name__)
+
+from odoo import api, models, fields
+from odoo.exceptions import Warning
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    @api.one
+    def action_generate_sale_order_link_tracker(self):
+        return super(SaleOrder, self).action_generate_sale_order_link_tracker()
+
+    @api.one
+    def action_send_sms_automatic(self, sms_template_id=False, need_check_date_order_send_sms=True):
+        #action_generate_sale_order_link_tracker
+        self.action_generate_sale_order_link_tracker()
+        #action_send_sms_automatic
+        return super(SaleOrder, self).action_send_sms_automatic(sms_template_id, need_check_date_order_send_sms)
+
+    @api.multi
+    def action_send_sms(self):
+        #action_generate_sale_order_link_tracker
+        for item in self:
+            item.action_generate_sale_order_link_tracker()
+        #action_send_sms
+        return super(SaleOrder, self).action_send_sms()
